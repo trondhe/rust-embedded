4415bb4eb97a9403a0c0506b463d9e24a0496a30
Trond H Emaus <trondhe@gmail.com>
2019-02-07 20:35:28 +0100

    do some cleaning

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,9 @@
 #![no_std]
 #![no_main]
-#[allow(unused)]
-use core::panic::PanicInfo;
-
-/// This function is called on panic.
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
-
-// use panic_halt;
-use nrf52832_pac;
-use nrf52832_hal::prelude;
 use cortex_m_rt::entry;
+#[allow(unused)]
+use panic_halt;
+// use nrf52832_hal::prelude;
 
 #[entry]
 fn main() -> ! {
